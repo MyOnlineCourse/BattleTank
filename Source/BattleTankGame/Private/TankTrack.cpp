4d9f3fbf0abd1c7c9e0fb49f86daab02f0c40365
TobyUE4 <36339200+TobyUE4@users.noreply.github.com>
2018-03-24 13:40:22 -0400

    Start movement controls
    FIX the KB/M movement LATER

---
+++ Source/BattleTankGame/Private/TankTrack.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTankGame.h"
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+
+	UE_LOG(LogTemp, Warning, TEXT("%s Current Throttle: %f"), *GetName(), Throttle);
+
+}
